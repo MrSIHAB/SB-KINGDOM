1b5f1aae3b6ea201f61ed5711feecf3716992254
SBsKingdom <hossainshiaib@gmail.com>
2023-10-01 20:34:54 +0600

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,46 +1,43 @@
+       // Create functions to fetch and embed the latest videos from both channels
+       async function fetchAndEmbedVideos(apiKey, channelId) {
+        try {
+            // Fetch the latest video from the specified channel using the YouTube Data API
+            const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&order=date&type=video&part=snippet&maxResults=1`);
+            const data = await response.json();
 
-// Diclaring Gloval variable
-const API_KEY = 'AIzaSyDOT8j31z0GKvX2E6OAlqi3KfgAPcG0TE0';
-const CHANNEL_ID = 'UCNdvDMh56hgSP4pepXgkwbA';
-const SN_CHANNEL_ID = 'UC4TW4FcIA6mdxZAy4cCoHlw';
+            // Extract the video ID
+            const videoId = data.items[0].id.videoId;
 
-
-// Create a function to fetch and embed the latest video
-async function embedLatestVideo(API_KEY, CHANNEL_ID){
-
-        // Fetch the latest video from your channel using the YouTube Data API
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&order=date&type=video&part=snippet&maxResults=10`);
-        const data = await response.json();
-
-        const videoid = data.items[0].id.videoId;
-
-        return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoid}" frameborder="0" allowfullscreen></iframe>`;
-
-}
-
-
-
-
-// making a if-else condision to get 10+10(20) video id
-async function extractingids(){
-
-        //condition
-        if (i=0, i<10, i++){
-            const embed1 = embedLatestVideo(API_KEY, CHANNEL_ID);
-            const embed2 = embedLatestVideo(API_KEY, SN_CHANNEL_ID);
-            embedcode += embed1+embed2;
+            // Generate the HTML iframe code for embedding the video
+            return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
+        } catch (error) {
+            console.error('Error:', error);
+            return '';
         }
-        
-        // Replacing embed code
-        const latestVideosDiv = document.getElementById('latest-videos');
-        latestVideosDiv.innerHTML = embedcode;
+    }
 
-        //errorcatch
+    // Create a function to embed alternating videos from both channels
+    async function embedAlternatingVideos() {
+        try {
+            // Initialize an empty string to store the combined HTML iframe code for all videos
+            let embedCode = '';
 
-}
+            // Fetch and embed videos from both channels alternately
+            for (let i = 0; i < 5; i++) {
+                const embed1 = await fetchAndEmbedVideos(API_KEY_CHANNEL_1, CHANNEL_ID_1);
+                const embed2 = await fetchAndEmbedVideos(API_KEY_CHANNEL_2, CHANNEL_ID_2);
+                embedCode += embed1 + embed2;
+            }
 
+            // Replace the content of the 'latest-videos' div with the combined embed code
+            const latestVideosDiv = document.getElementById('latest-videos');
+            latestVideosDiv.innerHTML = embedCode;
 
+            console.log('Embedded latest videos');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 
-
-// Call the function to embed the latest video when the page loads
-window.onload = embedLatestVideo;
+    // Call the function to embed alternating videos when the page loads
+    window.onload = embedAlternatingVideos;
